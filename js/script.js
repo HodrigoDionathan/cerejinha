71fbd11227769f64769eb2c25afb5c458e520873
Hodrigo Dionathan <hodrigodionatha@gmail.com>
2024-10-17 12:13:10 -0300

    atualizacao de fontes e ajuste de tamanhos
    atualização de tamanhos  de resolucao de tela e fontezs

--- js/script.js
+++ js/script.js
@@ -64,3 +64,57 @@ function startCherryRain() {
 function clearCherryRain() {
   cherryRainContainer.innerHTML = '';
 }
+
+const speechBubble = document.querySelector('.speech-bubble'); // Referência ao balão de fala
+
+function checkOrder() {
+  if (userOrder.length === 8) {
+    if (JSON.stringify(userOrder) === JSON.stringify(correctOrder)) {
+      lamp.src = 'imagens/lampada-acesa.png';
+      cerejinha.src = 'imagens/cerejinhafeliz.png';
+      cerejinha.style.transform = 'scale(2)';
+      message.textContent = 'Parabéns, você montou corretamente!';
+      speechBubble.textContent = 'Parabéns mandou Bem. Projeto Concluído.'; // Mensagem de sucesso
+      startCherryRain(); // Inicia a chuva de cerejas
+    } else {
+      message.textContent = 'TENTE DE NOVO MEU CHEFE!';
+      cerejinha.src = 'imagens/cerejinhaduvida.png';
+      cerejinha.style.transform = 'scale(2)';
+      speechBubble.textContent = 'Você Errou não Desista, Tente Novamente Meu Chefe!'; // Mensagem de erro
+    }
+  }
+}
+
+function resetGame() {
+  userOrder = [];
+  lamp.src = 'imagens/lampada-apagada.png';
+  cerejinha.src = 'imagens/cerejinhaduvida.png';
+  cerejinha.style.transform = 'scale(1)';
+  message.textContent = '';
+  speechBubble.textContent = 'Olá, eu sou o Cerejinha, me ajude a montar o Projeto do TCC!'; // Reset da mensagem
+  slots.forEach(slot => (slot.innerHTML = ''));
+  clearCherryRain(); // Limpa a chuva de cerejas
+}
+const componentNames = [
+  "Bateria",
+  "Transformador",
+  "Capacitor",
+  "Motor Elétrico",
+  "Eixo",
+  "Estator",
+  "Transformador",
+  "Transistor"
+];
+
+function resetGame() {
+  userOrder = [];
+  lamp.src = 'imagens/lampada-apagada.png';
+  cerejinha.src = 'imagens/cerejinhaduvida.png';
+  cerejinha.style.transform = 'scale(1)';
+  message.textContent = '';
+  speechBubble.textContent = 'Olá, eu sou o Cerejinha, me ajude a montar o Projeto do TCC!'; // Reset da mensagem
+  slots.forEach((slot, index) => {
+    slot.innerHTML = `<span class="component-name">${componentNames[index]}</span>`; // Reinserir nomes nas caixas
+  });
+  clearCherryRain(); // Limpa a chuva de cerejas
+}
